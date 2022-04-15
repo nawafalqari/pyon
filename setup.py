993d34b594689e0bcb714c01350b4b70a5bda2d5
nawafalqari <nawafalqari13@gmail.com>
2022-04-15 14:09:31 +0300

    Added project_urls to setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
 from setuptools import setup, find_packages
 
 # README.md
-with open('README.md', 'r') as readme_file:
+with open('README.md', 'r', encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
 setup(
     name='pyonr',
     packages=find_packages(),
-    version='1.0.0b3',
+    version='1.0.0b4',
     description='PYON Reader - Python Object Nation',
     author='Nawaf Alqari',
     author_email='nawafalqari13@gmail.com',
     keywords=['pyon', 'pyonr', 'json', 'pythonobjectnation', 'python object nation'],
     long_description=readme,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
+    project_urls={
+        'Documentation': 'https://github.com/nawafalqari/pyon#readme',
+        'Bug Tracker': 'https://github.com/nawafalqari/pyon/issues',
+        'Source Code': 'https://github.com/nawafalqari/pyon/',
+        'Discord': 'https://discord.gg/cpvynqk4XT'
+    },
     license='MIT',
     url='https://github.com/nawafalqari/pyon/',
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
