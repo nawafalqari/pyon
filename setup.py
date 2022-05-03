83feca9bbde69ce17a55bf6001e98b6796ecc0cb
nawafalqari <nawafalqari13@gmail.com>
2022-05-04 01:55:56 +0300

    Bug fixes

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import setup, find_packages
+import codecs
+import os.path
+
+# got these 2 functions from https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 # README.md
 with open('README.md', 'r', encoding='utf-8') as readme_file:
@@ -7,7 +23,7 @@ with open('README.md', 'r', encoding='utf-8') as readme_file:
 setup(
     name='pyonr',
     packages=find_packages(),
-    version='1.0.0b6',
+    version=get_version('pyonr/__init__.py'),
     description='PYON Reader - Python Object Nation',
     author='Nawaf Alqari',
     author_email='nawafalqari13@gmail.com',
